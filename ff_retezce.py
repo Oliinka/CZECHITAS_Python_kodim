edc12a3e0933d7b0c931649be09ff370ee4170ef
Oliinka <olga.titzenthaler@gmail.com>
2023-10-10 23:36:49 +0200

    PRAZDNY_SOUBOR_DOPRACOVAT!!!

---
+++ ff_retezce.py
@@ -0,0 +1,17 @@
+# cislovani v retezcich je od nuly prvni je 0 druhy je 1
+# cislovat lze od zadu -1
+
+jmeno = 'hubert'
+print(jmeno[0])  # 'h'
+print(jmeno[4])  # 'r'
+
+
+
+znamky = [2, 3, 4, 1, 1]
+teploty = [13.4, 13.5, 12.8, 11.9]
+uzivatele = ['mark', 'carl', 'eve', 'ellen']
+platby = ['Jan Novák', 12000, True]
+body = [[140, 120], [60, 92], [34, 68]]
+
+print(uzivatele[1][3])  # 'l' 
+print(body[2][0])  # 34
