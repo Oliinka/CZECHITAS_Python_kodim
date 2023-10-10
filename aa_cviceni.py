9386de66987747a592c6ad2c797db1e5de37ad99
Oliinka <olga.titzenthaler@gmail.com>
2023-10-10 12:51:50 +0200

    asi ano

---
+++ aa_cviceni.py
@@ -0,0 +1,17 @@
+#Vypište řetězec obsahující jméno divadla.
+jmeno_divadla = print("Jmeno Divadla")
+
+#Vypište řetězec obsahující jméno divadla tak, že sečtete dohromady jednotlivá slova toho jména.
+jmeno_1 = ("Narodni")
+jmeno_2 = ("Divadlo")
+
+print(jmeno_1 + " " + jmeno_2)
+
+#Zkuste vynásobit řetězec celým číslem. Jaký jste dostali výsledek?
+print(jmeno_divadla)
+
+#Vypište řetězec který vypadá takto: '111111000000', ale místo šesti jedniček a šesti nul obsahuje 256 jedniček a 256 nul.
+
+retezec = ("1"*10 + "0"*10)
+print(retezec)
+print('bla ' * 10)
