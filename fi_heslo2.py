8a91bdb5d6e7162c24b69a61fb94a1523eafe4d6
Oliinka <olga.titzenthaler@gmail.com>
2023-10-10 21:30:59 +0200

    nested loop

---
+++ fi_heslo2.py
@@ -0,0 +1,16 @@
+import random
+heslo = "heslo"
+pocet_pismen = len(heslo)
+
+for i in range (3):
+    nahodne_cislo = random.randint(1, pocet_pismen)
+    znak_hesla = input(f"Zadej {nahodne_cislo}. znak hesla: ")
+    if znak_hesla != heslo[(nahodne_cislo-1)]:
+        zamitnuty_vstup = True
+    else:
+        continue
+
+if zamitnuty_vstup==True:
+    print("Vstup zakázán.")
+else:
+    print("Vstup povolen.")
